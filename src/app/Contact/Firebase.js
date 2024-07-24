1294dffe7681201c84438adb98f196345317f8ff
hamza398 <60353843+hamza398@users.noreply.github.com>
2024-07-24 04:17:02 -0700

    new commits have been added

--- src/app/Contact/Firebase.js
+++ src/app/Contact/Firebase.js
@@ -1,18 +1,18 @@
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAeUV1MQ8FimJSyZB3IrEYNyDtzSoa4rdY",
-  authDomain: "contactpage-cc756.firebaseapp.com",
-  projectId: "contactpage-cc756",
-  storageBucket: "contactpage-cc756.appspot.com",
-  messagingSenderId: "984715667154",
-  appId: "1:984715667154:web:a30c99edeb57c1ab5fef0b",
-  measurementId: "G-TVFES3KK80"
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAeUV1MQ8FimJSyZB3IrEYNyDtzSoa4rdY",
+//   authDomain: "contactpage-cc756.firebaseapp.com",
+//   projectId: "contactpage-cc756",
+//   storageBucket: "contactpage-cc756.appspot.com",
+//   messagingSenderId: "984715667154",
+//   appId: "1:984715667154:web:a30c99edeb57c1ab5fef0b",
+//   measurementId: "G-TVFES3KK80"
    
-};
+// };
 
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
