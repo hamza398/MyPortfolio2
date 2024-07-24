5d564ac2140346707be839177c719cc8ad779a94
hamza398 <60353843+hamza398@users.noreply.github.com>
2024-07-24 04:29:40 -0700

    new commits have been added

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,34 +1,34 @@
-"use client";
-import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./Firebase";
-const Contact = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleData = async () => {
-    await createUserWithEmailAndPassword(auth, email, password);
-  };
-  return (
-    <div className="flex h-[100vh] w-[100vw] justify-center items-center">
-      <div className="form">
-        <input
-          placeholder="Message"
-          type="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          placeholder="Email"
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button onClick={handleData}>Submit the data </button>
-      </div>
-    </div>
-  );
-};
+// "use client";
+// import React, { useState } from "react";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "./Firebase";
+// const Contact = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const handleData = async () => {
+//     await createUserWithEmailAndPassword(auth, email, password);
+//   };
+//   return (
+//     <div className="flex h-[100vh] w-[100vw] justify-center items-center">
+//       <div className="form">
+//         <input
+//           placeholder="Message"
+//           type="email"
+//           onChange={(e) => {
+//             setEmail(e.target.value);
+//           }}
+//         />
+//         <input
+//           placeholder="Email"
+//           type="password"
+//           onChange={(e) => {
+//             setPassword(e.target.value);
+//           }}
+//         />
+//         <button onClick={handleData}>Submit the data </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Contact;
+// export default Contact;
