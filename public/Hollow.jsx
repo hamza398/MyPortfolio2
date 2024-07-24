020e7c0dd53682cf5d2262bbe7bd284c3748a261
hamza398 <60353843+hamza398@users.noreply.github.com>
2024-07-24 04:11:17 -0700

    new commits have been added

--- public/Hollow.jsx
+++ public/Hollow.jsx
@@ -2,18 +2,17 @@ import React, { useRef } from "react";
 import { Text, useGLTF } from "@react-three/drei";
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF("/hollow.glb");
+  
   return (
-    <group {...props} dispose={null} scale={0.74} position={[0, -1, 0]}>
-      <group>
+    
+    
       <Text position={[-0.3, 0, -0.5]} font="./Poppins-Black.ttf" lineHeight={1} textAlign="center" color="white" fontSize={1}>
           Hello my name is Hamza {'\n'} 
           I like making fun {'\n'}
           & interactive things with {'\n'} 
           code
         </Text>
-      </group>
-    </group>
+    
   );
 }
 
